cb72c3344bc2c927e63311181eb7aef95ee4f848
B3ardBr0 <wjmaritz@protonmail.com>
2025-03-05 11:55:35 +0300

    Configure site for GitHub Pages deployment

--- src/main.py
+++ src/main.py
@@ -1,26 +1,35 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"  # Changed from public to docs
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
-    print("Deleting public directory...")
+    # Get the basepath from command line arguments or default to "/"
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    print(f"Using basepath: {basepath}")
+    
+    print("Deleting docs directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-    print("Copying static files to public directory...")
+    print("Copying static files to docs directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
-main()
+if __name__ == "__main__":
+    main()
